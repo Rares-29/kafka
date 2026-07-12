47d301f8a6ae632a6115cf069aaa5e31b410ad32
Rares Sandu <raressandu52@gmail.com>
2026-07-12 07:58:36 +0300

    Flink finalized exercise for temperatures

--- src/main/java/com/rlearning/flink/TemperatureAverageJob.java
+++ src/main/java/com/rlearning/flink/TemperatureAverageJob.java
@@ -5,17 +5,33 @@ import org.apache.flink.table.api.*;
 import org.apache.flink.table.catalog.Catalog;
 import org.apache.flink.table.catalog.GenericInMemoryCatalog;
 
+import static org.apache.flink.table.api.Expressions.$;
+
 public class TemperatureAverageJob {
     public static void computeAverages() {
         EnvironmentSettings settings = EnvironmentSettings
                 .newInstance()
                 .inStreamingMode()
-                //.inBatchMode()
                 .build();
 
         TableEnvironment tableEnv = TableEnvironment.create(settings);
-        Catalog catalog = new GenericInMemoryCatalog("mem-cat");
-        tableEnv.registerCatalog("mem-cat", catalog);
-        tableEnv.executeSql("CREATE DATABASE mydb WITH (...)");
+
+        tableEnv.executeSql("""
+                CREATE TABLE SensorTemperature (
+                  `sensorId` INT,
+                  `temperature` INT,
+                  `room` STRING) WITH (
+                  'connector' = 'kafka',
+                  'topic' = 'sensor-temperature',
+                  'properties.bootstrap.servers' = 'localhost:9092',
+                  'properties.group.id' = 'testGroup',
+                  'scan.startup.mode' = 'earliest-offset',
+                  'format' = 'json',
+                  'json.ignore-parse-errors' = 'true'
+                )""");
+        Table table = tableEnv.from("SensorTemperature");
+        Table result = table.groupBy($("room"))
+                .select($("room"), $("temperature").avg().as("avg_temperature"));
+        result.execute().print();
     }
 }
